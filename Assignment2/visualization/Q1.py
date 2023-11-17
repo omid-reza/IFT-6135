b66a448f492db8c1ebddbb154dcaa0eebbc409fe
Omid Reza Heidari <omid.orh@gmail.com>
2023-11-17 00:48:24 -0500

    Create Q1.py

---
+++ Assignment2/visualization/Q1.py
@@ -0,0 +1,35 @@
+from matplotlib import pyplot as plt
+import json
+
+executed_configs = {
+    1:"01-lstm_layer_1_btch_16_adam",
+    2:"02-lstm_layer_1_btch_16_adamw",
+    3:"03-lstm_layer_1_btch_16_sgd",
+    9:"09-lstm_layer_2_btch_16_adamw",
+    10:"10-lstm_layer_4_btch_16_adamw"
+}
+
+for x in executed_configs:
+    folder_name = executed_configs[x]
+    args = json.load(open(f"../LoggedData/TrVlTs/{folder_name}/args.json", "r"))
+    with open(f"../LoggedData/TrVlTs/{folder_name}/train_time.txt", "r") as train_file:
+        data = l = [float(line) for line in train_file]
+        plt.plot(data, label=f"{args['model']} | layers:{args['layers']} | btch:{args['batch_size']}")
+plt.title("Train")
+plt.ylabel("Time (Second)")
+plt.xlabel("Epochs")
+plt.legend(loc="upper right")
+plt.savefig("EpochTime(Tr).png")
+
+for x in executed_configs:
+    folder_name = executed_configs[x]
+    args = json.load(open(f"../LoggedData/TrVlTs/{folder_name}/args.json", "r"))
+    with open(f"../LoggedData/TrVlTs/{folder_name}/valid_time.txt", "r") as validation_file:
+        data = l = [float(line) for line in validation_file]
+        plt.plot(data, label=f"{args['model']} | layers:{args['layers']} | btch:{args['batch_size']}")
+plt.title("Validation")
+plt.ylabel("Time (Second)")
+plt.xlabel("Epochs")
+plt.legend(loc="upper right")
+plt.savefig("EpochTime(Vl).png")
+#%%
