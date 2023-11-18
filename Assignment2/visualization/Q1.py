3b9c92bd647c3742465a6ac63e70194f09095512
Omid Reza Heidari <omid.orh@gmail.com>
2023-11-17 19:40:42 -0500

    Reshape plots and separate them

--- Assignment2/visualization/Q1.py
+++ Assignment2/visualization/Q1.py
@@ -1,6 +1,5 @@
 import json
 from matplotlib import pyplot as plt
-from statistics import mean
 import numpy as np
 
 executed_configs = {
@@ -10,62 +9,49 @@ executed_configs = {
     4:"04-lstm_layer_1_btch_16_momentum",
     5:"05-gpt1_layer_1_btch_16_adam",
     6:"06-gpt1_layer_1_btch_16_adamw",
+    7:"07-gp1_layer_1_btch_16_sgd",
+    8:"08-gpt1_layer_1_btch_16_momentum",
     9:"09-lstm_layer_2_btch_16_adamw",
     10:"10-lstm_layer_4_btch_16_adamw",
     11:"11-gpt1_layer_2_btch_16_adamw",
+    12:"12-gpt1_layer_4_btch_16_adamw"
 }
-
+plt.figure(figsize=(12, 40))
 for x in executed_configs:
+    plt.subplot(6, 2, x)
     folder_name = executed_configs[x]
     args = json.load(open(f"../LoggedData/TrVlTs/{folder_name}/args.json", "r"))
     with open(f"../LoggedData/TrVlTs/{folder_name}/train_ppl.txt", "r") as train_file:
         data = l = [float(line) for line in train_file]
-        plt.plot(np.cumsum(data), label=f"{args['model']} | layers:{args['layers']} | btch:{args['batch_size']} | optimizer:{args['optimizer']}")
-plt.title("Train")
-plt.ylabel("PPL")
-plt.xlabel("Epochs")
-plt.legend(loc="upper right")
-plt.savefig("EpochPPL(Tr).png")
-
-plt.cla()
-for x in executed_configs:
-    folder_name = executed_configs[x]
-    args = json.load(open(f"../LoggedData/TrVlTs/{folder_name}/args.json", "r"))
+        plt.plot(np.cumsum(data), label="train")
     with open(f"../LoggedData/TrVlTs/{folder_name}/valid_ppl.txt", "r") as validation_file:
         data = l = [float(line) for line in validation_file]
-        plt.plot(np.cumsum(data), label=f"{args['model']} | layers:{args['layers']} | btch:{args['batch_size']} | optimizer:{args['optimizer']}")
-plt.title("Validation")
-plt.ylabel("PPL")
-plt.xlabel("Epochs")
-plt.legend(loc="upper right")
-plt.savefig("EpochPPL(Vl).png")
+        plt.plot(np.cumsum(data), label="validation")
+    plt.title(f"Experiment {x}")
+    plt.ylabel("PPL")
+    plt.xlabel("Epochs")
+    plt.legend(loc="upper right")
+plt.savefig("EpochPPL.png")
 
 plt.cla()
+plt.figure(figsize=(12, 40))
 for x in executed_configs:
     folder_name = executed_configs[x]
+    plt.subplot(6, 2, x)
     args = json.load(open(f"../LoggedData/TrVlTs/{folder_name}/args.json", "r"))
     with open(f"../LoggedData/TrVlTs/{folder_name}/train_ppl.txt", "r") as train_ppl_file:
         with open(f"../LoggedData/TrVlTs/{folder_name}/train_time.txt", "r") as train_time_file:
             data = [float(line) for line in train_ppl_file]
             times = [float(line) for line in train_time_file]
-            plt.plot(np.cumsum(times), data, label=f"{args['model']} | layers:{args['layers']} | btch:{args['batch_size']} | optimizer:{args['optimizer']}")
-plt.title("Train")
-plt.ylabel("PPL")
-plt.xlabel("Time (Second)")
-plt.legend(loc="upper right")
-plt.savefig("TrainTime.png")
-
-plt.cla()
-for x in executed_configs:
-    folder_name = executed_configs[x]
-    args = json.load(open(f"../LoggedData/TrVlTs/{folder_name}/args.json", "r"))
+            plt.plot(np.cumsum(times), data, label=f"train")
     with open(f"../LoggedData/TrVlTs/{folder_name}/valid_ppl.txt", "r") as valid_ppl_file:
         with open(f"../LoggedData/TrVlTs/{folder_name}/valid_time.txt", "r") as valid_time_file:
             data = [float(line) for line in valid_ppl_file]
             times = [float(line) for line in valid_time_file]
-            plt.plot(np.cumsum(times), data, label=f"{args['model']} | layers:{args['layers']} | btch:{args['batch_size']} | optimizer:{args['optimizer']}")
-plt.title("Validation")
-plt.ylabel("PPL")
-plt.xlabel("Time (Second)")
-plt.legend(loc="upper right")
-plt.savefig("ValidationTime.png")
+            plt.plot(np.cumsum(times), data, label=f"validation")
+    plt.title(f"Experiment {x}")
+    plt.ylabel("PPL")
+    plt.xlabel("Time (Second)")
+    plt.legend(loc="upper right")
+plt.savefig("TimePPL.png")
+#%%
