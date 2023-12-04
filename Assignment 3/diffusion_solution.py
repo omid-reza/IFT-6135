fb764310142f8ffdc8e4c8c47c4c76adca2566ed
Omid Reza Heidari <omid.orh@gmail.com>
2023-12-03 22:07:24 -0500

    Complete the class

--- Assignment 3/diffusion_solution.py
+++ Assignment 3/diffusion_solution.py
@@ -86,7 +86,7 @@ def p_sample_loop(model, shape, timesteps, T, coefficients, noise=None):
         imgs = []
 
         for i in tqdm(reversed(range(0, timesteps)), desc='Sampling', total=T, leave=False):
-            img = None
+            img = p_sample(model=model, x=img, t=torch.full((b,), i, device=model.device, dtype=torch.long), t_index=i, coefficients=coefficients, noise=noise[i+1])
             imgs.append(img.cpu())
 
         return torch.stack(imgs)
