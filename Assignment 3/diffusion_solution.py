c5f662826f78676b7f40cbdeeb0e013e7845c9e9
Omid Reza Heidari <omid.orh@gmail.com>
2023-12-07 13:58:19 -0500

    Update diffusion_solution.py

--- Assignment 3/diffusion_solution.py
+++ Assignment 3/diffusion_solution.py
@@ -84,11 +84,11 @@ def p_sample_loop(model, shape, timesteps, T, coefficients, noise=None):
     with torch.no_grad():
         b = shape[0]
         # Start from pure noise (x_T)
-        img = torch.randn(shape, device=device) if noise is None else noise[0]
+        img = torch.randn(shape, device="cuda") if noise is None else noise[0]
         imgs = []
 
         for i in tqdm(reversed(range(0, timesteps)), desc='Sampling', total=T, leave=False):
-            img = p_sample(model=model, x=img, t=torch.full((b,), i, device=device, dtype=torch.long), t_index=i, coefficients=coefficients, noise=noise[i+1])
+            img = p_sample(model=model, x=img, t=torch.full((b,), i, device="cuda", dtype=torch.long), t_index=i, coefficients=coefficients, noise=noise[i+1])
             imgs.append(img.cpu())
 
         return torch.stack(imgs)
