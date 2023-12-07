0b9e11a86cb456419220623e3752e36417d65798
Omid Reza Heidari <omid.orh@gmail.com>
2023-12-07 13:53:59 -0500

    Update diffusion_solution.py

--- Assignment 3/diffusion_solution.py
+++ Assignment 3/diffusion_solution.py
@@ -26,7 +26,7 @@ def extract(a, t, x_shape):
 
 def alphas_betas_sequences_helper(beta_start, beta_end, T):
     def linear_beta_schedule(beta_start, beta_end, timesteps):
-        return torch.linspace(beta_start, beta_end, timesteps).to(device)
+        return torch.linspace(beta_start, beta_end, timesteps)
 
     betas = linear_beta_schedule(beta_start, beta_end, T)
     alphas = 1 - betas
