b50a6de0b24aa039fe4863e655c3ffc05c40cfce
Omid Reza Heidari <omid.orh@gmail.com>
2023-12-07 14:23:05 -0500

    Update diffusion_solution.py

--- Assignment 3/diffusion_solution.py
+++ Assignment 3/diffusion_solution.py
@@ -19,7 +19,7 @@ def extract(a, t, x_shape):
     #         out[i] contains a[t[i]]
     
     batch_size = t.shape[0]
-    out = a.gather(-1, t.gpu())
+    out = a.gather(-1, t)
     return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
 
 
