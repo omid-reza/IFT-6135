7522c153d8837257e62db9bff173d85682e249e3
Omid Reza Heidari <omid.orh@gmail.com>
2023-12-08 12:15:09 -0500

    Update VAE.py

--- Assignment 3/VAE.py
+++ Assignment 3/VAE.py
@@ -137,7 +137,7 @@ from vae_solution import interpolate
 
 from vae_solution import interpolate
 
-if name == 'main':
+if __name__ == 'main':
     z_1 = torch.randn(1, z_dim).to(device)
     z_2 = torch.randn(1, z_dim).to(device)
 
